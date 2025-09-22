bc2f14ed50a2a220bd946394a444ec80db3e5b7a
Luke Blaney <git@lukeblaney.co.uk>
2025-09-22 20:23:52 +0100

    set schema for items collection in typesense on startup

--- search/entrypoint.sh
+++ search/entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 : "${PORT:?Can\'t find environment variable PORT}"
 : "${TYPESENSE_ADMIN_KEY:?Can\'t find environment variable TYPESENSE_ADMIN_KEY}"
 
+
+
+TYPESENSE_URL="http://localhost:${PORT}"
+
 # Start Typesense in background
 GLOG_minloglevel=1 /opt/typesense-server --data-dir /data \
 	--api-key="$TYPESENSE_ADMIN_KEY" \
@@ -13,7 +17,7 @@ TYPESENSE_PID=$!
 
 # Wait for the API to be ready
 echo "Waiting for Typesense API..."
-until curl "http://localhost:${PORT}/health" --silent --fail > /dev/null; do
+until curl "${TYPESENSE_URL}/health" --silent --fail > /dev/null; do
 	sleep 1
 done
 
@@ -32,7 +36,7 @@ if [ -n "$CLIENT_KEYS" ]; then
 fi
 
 # Fetch existing keys
-EXISTING_KEYS_JSON=$(curl -s "http://localhost:${PORT}/keys" \
+EXISTING_KEYS_JSON=$(curl -s "${TYPESENSE_URL}/keys" \
 	-H "X-TYPESENSE-API-KEY: ${TYPESENSE_ADMIN_KEY}")
 EXISTING_SYSTEMS=$(echo "$EXISTING_KEYS_JSON" | jq -r '.keys[]?.description')
 
@@ -56,9 +60,9 @@ for SYSTEM_ENV in "${!DESIRED_KEYS[@]}"; do
 		if [ "$CURRENT_ACTIONS" != "$ACTIONS" ] || [[ "$KEY" != "$CURRENT_KEY_PREFIX"*  ]]; then
 			echo "Updating key for $SYSTEM_ENV with actions $ACTIONS"
 			KEY_ID=$(echo "$EXISTING_KEYS_JSON" | jq -r ".keys[] | select(.description==\"$SYSTEM_ENV\") | .id")
-			curl -X DELETE "http://localhost:${PORT}/keys/${KEY_ID}" \
+			curl -X DELETE "${TYPESENSE_URL}/keys/${KEY_ID}" \
 				-H "X-TYPESENSE-API-KEY: ${TYPESENSE_ADMIN_KEY}" --silent --show-error --fail > /dev/null
-			curl "http://localhost:${PORT}/keys" \
+			curl "${TYPESENSE_URL}/keys" \
 				-H "X-TYPESENSE-API-KEY: ${TYPESENSE_ADMIN_KEY}" \
 				-H "Content-Type: application/json" \
 				-d "$PAYLOAD_JSON" --silent --show-error --fail > /dev/null
@@ -67,7 +71,7 @@ for SYSTEM_ENV in "${!DESIRED_KEYS[@]}"; do
 		fi
 	else
 		echo "Creating key for $SYSTEM_ENV with actions $ACTIONS"
-		curl "http://localhost:${PORT}/keys" \
+		curl "${TYPESENSE_URL}/keys" \
 			-H "X-TYPESENSE-API-KEY: ${TYPESENSE_ADMIN_KEY}" \
 			-H "Content-Type: application/json" \
 			-d "$PAYLOAD_JSON" --silent --show-error --fail > /dev/null
@@ -79,9 +83,33 @@ for EXISTING_SYSTEM in $EXISTING_SYSTEMS; do
 	if [ -z "${DESIRED_KEYS[$EXISTING_SYSTEM]}" ]; then
 		KEY_ID=$(echo "$EXISTING_SYSTEMS_JSON" | jq -r ".keys[] | select(.description==\"$EXISTING_SYSTEM\") | .id")
 		echo "Revoking stale key ($EXISTING_SYSTEM)"
-		curl -X DELETE "http://localhost:${PORT}/keys/${KEY_ID}" \
+		curl -X DELETE "${TYPESENSE_URL}/keys/${KEY_ID}" \
 			-H "X-TYPESENSE-API-KEY: ${TYPESENSE_ADMIN_SYSTEM}" --silent --show-error --fail > /dev/null
 	fi
 done
+
+# Check if collection already exists
+if curl -s -H "X-TYPESENSE-API-KEY: ${TYPESENSE_ADMIN_KEY}" \
+	"${TYPESENSE_URL}/collections/items" | grep -q '"name":"items"'; then
+	echo "Collection 'items' already exists, skipping schema creation and overrides."
+else
+	echo "Creating 'items' collection..."
+	curl -X POST "${TYPESENSE_URL}/collections" \
+		-H "X-TYPESENSE-API-KEY: ${TYPESENSE_ADMIN_KEY}" \
+		-H "Content-Type: application/json" \
+		-d '{
+			"name": "items",
+			"fields": [
+				{"name": "type", "type": "string", "facet": true},
+				{"name": "pref_label", "type": "string", "full_text_search": true, "sort": true},
+				{"name": "labels", "type": "string[]", "optional": true, "full_text_search": true},
+				{"name": "description", "type": "string", "optional": true, "full_text_search": true},
+				{"name": "lyrics", "type": "string", "optional": true, "full_text_search": true}
+			],
+			"default_sorting_field":"pref_label"
+		}' --silent --show-error --fail-with-body
+	echo -e "\nCollection 'items' created."
+fi
+
 
 wait $TYPESENSE_PID
