aaaf69a0d43be9c7fe89e44562c899a56ce6cb4c
Luke Blaney <git@lukeblaney.co.uk>
2025-09-22 20:28:14 +0100

    update ingestor to also populate search index

--- ingestor/ingest.py
+++ ingestor/ingest.py
@@ -7,6 +7,28 @@ import os
 import sys
 import requests
 import re
+from rdflib import Graph, Namespace, RDF, RDFS, Literal
+import typesense
+
+# Common namespaces
+SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
+DC = Namespace("http://purl.org/dc/terms/")
+MO = Namespace("http://purl.org/ontology/mo/")
+
+# Hardcoded type mapping
+TYPE_LABELS = {
+	"http://xmlns.com/foaf/0.1/Person": "Person",
+	"http://purl.org/ontology/mo/Track": "Track",
+	"http://www.w3.org/2006/time#DayOfWeek": "Day of Week",
+	"http://www.w3.org/2006/time#MonthOfYear": "Month of Year",
+	"https://dbpedia.org/ontology/MeanOfTransportation": "Means of Transport",
+	"http://www.w3.org/2002/07/owl#ObjectProperty": "Object Property",
+	"http://www.w3.org/2002/07/owl#Class": "Class",
+	"http://www.w3.org/2000/01/rdf-schema#Class": "Class",
+	"http://www.w3.org/2002/07/owl#DatatypeProperty": "Datatype Property",
+	"http://www.w3.org/2002/07/owl#Ontology": "Ontology",
+}
+
 
 SCHEDULE_TRACKER_ENDPOINT = os.environ.get("SCHEDULE_TRACKER_ENDPOINT")
 LOGANNE_ENDPOINT = os.environ.get("LOGANNE_ENDPOINT")
@@ -101,6 +123,93 @@ def cleanup_triplestore(graph_uris):
 			)
 
 
+def get_type_label(graph, type_uri):
+	uri_str = str(type_uri)
+	if uri_str in TYPE_LABELS:
+		return TYPE_LABELS[uri_str]
+
+	# Try dynamic lookup
+	for label in graph.objects(type_uri, SKOS.prefLabel):
+		return str(label)
+
+	raise ValueError(f"Unknown type URI encountered: {uri_str}")
+
+
+def graph_to_typesense_docs(graph: Graph):
+	"""
+	Convert an RDFLib Graph into a list of documents
+	ready for indexing in Typesense.
+	"""
+	docs = {}
+
+	for subj in set(graph.subjects()):
+		doc = {
+			"id": str(subj),
+			"type": None,
+			"pref_label": None,
+			"labels": [],
+			"description": None,
+			"lyrics": None,
+		}
+
+		# type (exactly one, from hardcoded mapping only)
+		for o in graph.objects(subj, RDF.type):
+			doc["type"] = get_type_label(graph, o)
+			break
+
+		# pref_label
+		for o in graph.objects(subj, SKOS.prefLabel):
+			if isinstance(o, Literal):
+				doc["pref_label"] = str(o)
+				break
+
+		# labels (can be multiple)
+		for o in graph.objects(subj, RDFS.label):
+			if isinstance(o, Literal):
+				doc["labels"].append(str(o))
+
+		# description
+		for o in graph.objects(subj, DC.description):
+			if isinstance(o, Literal):
+				doc["description"] = str(o)
+				break
+
+		# lyrics
+		for o in graph.objects(subj, MO.lyrics):
+			if isinstance(o, Literal):
+				doc["lyrics"] = str(o)
+				break
+
+		# only include if we have a type and pref_label
+		if doc["type"] and doc["pref_label"]:
+			docs[doc["id"]] = doc
+
+	return list(docs.values())
+
+
+typesense_client = typesense.Client({
+    "nodes": [{
+        "host": "search",
+        "port": "8108",
+        "protocol": "http",
+    }],
+    "api_key": KEY_LUCOS_ARACHNE,
+    "connection_timeout_seconds": 2
+})
+
+
+def update_searchindex(system, content, content_type):
+	if not system.startswith("lucos_"):
+		return
+	g = Graph()
+	g.parse(data=content, format=content_type)
+	docs = graph_to_typesense_docs(g)
+	results = typesense_client.collections["items"].documents.import_(docs, {"action": "upsert"})
+	for result in results:
+		if not result["success"]:
+			raise Error(f"Error returned from search index upsert: {result["error"]}")
+	print(f"Upserted {len(results)} documents to triplestore from {system}")
+
 if __name__ == "__main__":
 	try:
 		urls = {
@@ -120,6 +229,7 @@ if __name__ == "__main__":
 		for system, url in urls.items():
 			(content, content_type) = fetch_url(system, url)
 			update_triplestore(url, content, content_type)
+			update_searchindex(system, content, content_type)
 		cleanup_triplestore(urls.values())
 
 		# Loganne
