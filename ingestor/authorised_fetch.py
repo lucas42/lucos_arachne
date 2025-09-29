4e480ebd444fe8b337d640fe71ad3bd368900655
Luke Blaney <git@lukeblaney.co.uk>
2025-09-29 17:32:06 +0100

    Send auth headers to fetches after redirect in ingestor

--- ingestor/authorised_fetch.py
+++ ingestor/authorised_fetch.py
@@ -19,19 +19,34 @@ def fetch_url(system, url):
 		auth_header = {"Authorization": f"key {key}"}
 
 	# In dev environment, where URLs can be referencing localhost, switch domain to the docker internal domain to allow requests between containers
-	if url.startswith("http://localhost:"):
-		url = url.replace("http://localhost:", "http://host.docker.internal:")
+	def map_localhost(url) -> str:
+		if url.startswith("http://localhost:"):
+			return url.replace("http://localhost:", "http://host.docker.internal:")
+		return url
 
+	url = map_localhost(url)
 	print(f"Ingesting data from {url}")
 
 	# Fetch data
 	resp = session.get(
 		url,
-		headers={
-			**auth_header,
-		},
-		allow_redirects=True,
+		headers={**auth_header},
+		allow_redirects=False,
 	)
+
+	# Do first redirect manually, so auth header can be sent to next url
+	if resp.is_redirect or resp.is_permanent_redirect:
+		redirect_url = resp.headers["Location"]
+		redirect_url = requests.compat.urljoin(resp.url, redirect_url)
+		redirect_url = map_localhost(redirect_url)
+		print(f"Following redirect to {redirect_url}")
+
+		resp = session.get(
+			redirect_url,
+			headers={**auth_header},
+			allow_redirects=True,
+		)
+
 	resp.raise_for_status()
 	content = resp.text
 
