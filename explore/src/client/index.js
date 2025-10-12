7d786f37064bd997e4ffb6af5eef2a86226ee182
Luke Blaney <git@lukeblaney.co.uk>
2025-10-12 01:13:23 +0100

    add basic search ui to explore module

--- explore/src/client/index.js
+++ explore/src/client/index.js
@@ -2,17 +2,21 @@ import 'lucos_navbar';
 import Yasgui from "@zazuko/yasgui";
 import "@zazuko/yasgui/build/yasgui.min.css";
 
-// yasgui includes any search params in requests to sparql endpoint, so ensure there are none
-if (window.location.search) {
-	window.history.pushState(true, null, window.location.pathname);
-}
 const yasgui_container = document.getElementById("yasgui");
-new Yasgui(yasgui_container, {
-	requestConfig: {
-		endpoint: window.location.origin+"/sparql",
-		headers: () => ({
-			Authorization: `basic ${yasgui_container.dataset.auth}`,
-		}),
-	},
-	copyEndpointOnNewTab: false,
-});
+if (yasgui_container) {
+
+	// yasgui includes any search params in requests to sparql endpoint, so ensure there are none
+	if (window.location.search) {
+		window.history.pushState(true, null, window.location.pathname);
+	}
+
+	new Yasgui(yasgui_container, {
+		requestConfig: {
+			endpoint: window.location.origin+"/sparql",
+			headers: () => ({
+				Authorization: `basic ${yasgui_container.dataset.auth}`,
+			}),
+		},
+		copyEndpointOnNewTab: false,
+	});
+}
--- explore/src/server/index.js
+++ explore/src/server/index.js
@@ -14,6 +14,33 @@ app.get('/', catchErrors(async (req, res) => {
 		sparql_auth: Buffer.from(`lucos_arachne:${process.env.KEY_LUCOS_ARACHNE}`).toString('base64'),
 	});
 }));
+app.get('/search', catchErrors(async (req, res) => {
+	const q = req.query.q;
+	if (!req.query.q) {
+		res.redirect(302, '/explore');
+		return;
+	}
+	const searchParams = new URLSearchParams({
+		q: req.query.q,
+		query_by: "pref_label,labels,description,lyrics",
+		query_by_weights: "10,8,3,1",
+		include_fields: "id,pref_label,type,labels",
+		sort_by: "_text_match:desc,pref_label:asc",
+		prioritize_num_matching_fields: false,
+		enable_highlight_v1: false,
+		highlight_start_tag: '<span class="highlight">',
+		highlight_end_tag: '</span>',
+	});
+	const response = await fetch("http://web:8033/search?"+searchParams.toString(), {
+		headers: { 'X-TYPESENSE-API-KEY': process.env.KEY_LUCOS_ARACHNE },
+		signal: AbortSignal.timeout(900),
+	});
+	const data = await response.json();
+	if (!response.ok) {
+		throw new Error(`Recieved ${response.status} error from search endpoint: ${data["message"]}`);
+	}
+	res.render('search', data);
+}));
 
 // Error Handler
 app.use((error, req, res, next) => {
